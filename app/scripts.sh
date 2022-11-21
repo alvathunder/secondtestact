1b2af63101a3af6b0731cfc7eee3b1fbba59bf7e
alvathunder <87148598+alvathunder@users.noreply.github.com>
2022-11-22 00:19:44 +0100

    Update scripts.sh

--- app/scripts.sh
+++ app/scripts.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 sudo apt update
 sudo apt upgrade
-sudo apt install aws-cli
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
 aws --version
-
